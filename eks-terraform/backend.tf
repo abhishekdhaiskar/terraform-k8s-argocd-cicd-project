db3f1043f02783ff214e5b5ebd333c2d80273b95
abhishekdhaiskar <120624413+abhishekdhaiskar@users.noreply.github.com>
2025-06-28 12:51:38 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "nareshawsdevv"
+    bucket = "nareshawsdevvabhishek-project-west2"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
