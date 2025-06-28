5c1040c1648c15ed3b36aa57db5b016b468ffa66
abhishekdhaiskar <120624413+abhishekdhaiskar@users.noreply.github.com>
2025-06-28 13:45:11 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "nareshawsdevvabhishek-project-west2"
     key    = "eks/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-west-2"
   }
 
   required_version = ">= 1.6.3"
